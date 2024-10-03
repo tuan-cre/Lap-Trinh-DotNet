16e22b2cd71122c678402e439e9e2642671e8537
tuan-cre <lhtuan7924@gmail.com>
2024-10-03 09:54:08 +0700

    QUICK

--- BuoiTH1/Bai7/obj/Release/net8.0/Bai7.AssemblyInfo.cs
+++ BuoiTH1/Bai7/obj/Release/net8.0/Bai7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93068aef7f8a57d4eeb4f3f40095dcbb4b60dce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef75192f1e494e5b2b7055fade86a24168b66de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
